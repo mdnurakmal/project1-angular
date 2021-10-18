1c35fcbab692e773ff785237da73405c1b2a8a65
Nur Akmal <mdnurakmal@gmail.com>
2021-10-19 01:56:59 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x #echo onnano
+
+#echo "hello"
+#temp=$(kubectl get services kafka -o=jsonpath='{.spec.clusterIP}')
+#echo "running" $temp
+#sed -i "s/clusterip/$temp/" ./configmap.yaml
+
+#SPRING_CONFIG_LOCATION=`echo $PWD"/configmap.yaml"`
+
+kubectl apply -f ./configmap.yaml
+kubectl apply -f ./deployment.yaml
+kubectl rollout restart deployment/angular
