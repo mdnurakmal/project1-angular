2e82a4a66461289f338476d4454d2df447096339
Nur Akmal <mdnurakmal@gmail.com>
2021-10-19 12:15:33 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-sed -i "s#REPLACED_BY_BUILD_SCRIPT_AUTHSERVER_URL#$AUTHSERVER_URL#g" /usr/share/nginx/html/main.*.bundle.js
+sed -i "s#REPLACED_BY_BUILD_SCRIPT_AUTHSERVER_URL#$AUTHSERVER_URL#g" /usr/share/nginx/html/main.js
 exec "$@"
