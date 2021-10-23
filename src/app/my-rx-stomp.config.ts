f4c3f16c2dfc37cc74c8d51124cd58aa486a8ca4
Nur Akmal <mdnurakmal@gmail.com>
2021-10-23 17:30:35 +0800

    Update my-rx-stomp.config.ts

--- src/app/my-rx-stomp.config.ts
+++ src/app/my-rx-stomp.config.ts
@@ -2,7 +2,7 @@ import { InjectableRxStompConfig } from '@stomp/ng2-stompjs';
 
 export const myRxStompConfig: InjectableRxStompConfig = {
   // Which server?
-  brokerURL: 'http://wschat.mdnurakmal.com',
+  brokerURL: 'ws://wschat.mdnurakmal.com',
 
   // Headers
   // Typical keys: login, passcode, host
