32eedfa63dbfa40f58075333046bfd26c90f58d8
Nur Akmal <mdnurakmal@gmail.com>
2021-10-26 01:53:57 +0800

    Update chat.component.ts

--- src/app/modules/chat/chat.component.ts
+++ src/app/modules/chat/chat.component.ts
@@ -57,7 +57,7 @@ export class ChatComponent implements OnInit {
 
       }
 
-      this.rxStompService.publish({ destination: this.getAllMessagesTopic, body: "" });
+      this.rxStompService.publish({ destination: this.getAllMessagesTopic, body: "getAllMessagesTopic" });
     }
     } );
   }
